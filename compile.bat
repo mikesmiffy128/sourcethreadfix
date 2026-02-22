99c5fff1588d131b5f104d1b7404874d9cdd6f35
Michael Smith <mikesmiffy128@gmail.com>
2026-02-22 23:30:26 +0000

    Get rid of -dynamicbase to reduce size by ~3KiB

--- compile.bat
+++ compile.bat
@@ -5,8 +5,8 @@ llvm-rc /nologo /r /fo tmp.res src/exe.rc
 clang-cl -m32 -mno-sse -fuse-ld=lld -flto -O1 -GR- -GS- -Gs9999999 -EHa- -Oi ^
 -Fehl2.wrap.exe -W3 -Wpedantic -Wno-parentheses -DWIN32_LEAN_AND_MEAN -DNOMINMAX ^
 src/wincrt.c src/wrap.c src/x86.c ^
--link -nodefaultlib -subsystem:windows,6.0 -stack:0x10000,0x10000 -fixed:no ^
--dynamicbase -Brepro ^
+-link -nodefaultlib -subsystem:windows,6.0 -stack:0x10000,0x10000 -fixed ^
+-Brepro ^
 kernel32.lib user32.lib tmp.res
 del tmp.res & exit /b %errorlevel%
 
